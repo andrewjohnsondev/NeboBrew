02bdc52970b492e6b640a6e7d2af670bdb681f2e
Andrew Johnson <itsandyjohn@gmail.com>
2022-10-25 23:08:09 -0600

    Force build

--- pages/api/webhooks.js
+++ pages/api/webhooks.js
@@ -13,7 +13,6 @@ export const config = {
 export default async function webhookHandler(req, res) {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
   if (req.method === 'POST') {
-    console.log('test from backend');
     const buf = await buffer(req);
     const sig = req.headers['stripe-signature'];
     const webhookSecret = process.env.STRIPE_WEBHOOK_SIGNING_SECRET;
