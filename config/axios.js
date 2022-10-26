6f840267d6cbd16776b529bbd0295779baba14b5
Andrew Johnson <itsandyjohn@gmail.com>
2022-10-25 21:55:37 -0600

    fixed env

--- config/axios.js
+++ config/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_SANITY_URL,
-  headers: { Authorization: `Bearer ${process.env.NEXT_PUBLIC_NEBOBREW_API_KEY}` },
+  headers: { Authorization: `Bearer ${process.env.SANITY_AUTH_TOKEN}` },
 });
 
 export default instance;
