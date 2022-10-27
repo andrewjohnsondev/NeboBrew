90f1d7b3cd40ae6049a4ccfb5615389a8e6175cc
Andrew Johnson <itsandyjohn@gmail.com>
2022-10-27 01:32:01 -0600

    test

--- pages/api/stripe/subscription.js
+++ pages/api/stripe/subscription.js
@@ -49,7 +49,7 @@ export default async function handler(req, res) {
 
       res.status(200).json(session);
     } catch (err) {
-      res.status(err.statusCode || 500).json('happended here');
+      res.status(err.statusCode || 500).json(err.message);
     }
   } else {
     res.setHeader('Allow', 'POST');
