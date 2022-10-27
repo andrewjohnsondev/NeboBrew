40304053a73fc7dedc4c749a9d953628d023dafd
Andrew Johnson <itsandyjohn@gmail.com>
2022-10-27 15:08:50 -0600

    remove webhook folder

--- pages/api/webhooks.js
+++ pages/api/webhooks.js
@@ -122,7 +122,23 @@ export default async function webhookHandler(req, res) {
       }
       if (event.type === 'customer.subscription.deleted') {
         res.status(200).send();
-        await deleteSubscription(eventObject);
+        const id = eventObject.id;
+        const { data } = await axios.get(`${process.env.SANITY_URL}?query=*%5B_type%20%3D%3D%20'subscriptions'%20%26%26%20subscriptionId%20%3D%3D%20'${id}'%5D%7B%0A%20_id%0A%20%20%7D%20%20`);
+
+        const subscriptionId = data.result[0]._id;
+
+        await neboAxios.post('/', {
+          mutations: [
+            {
+              patch: {
+                id: subscriptionId,
+                set: {
+                  active: false,
+                },
+              },
+            },
+          ],
+        });
       }
     }
 
