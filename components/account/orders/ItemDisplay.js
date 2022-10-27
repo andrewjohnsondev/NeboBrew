2da6a2cc1b8d3853a9e86ad1856d850737a9935a
Andrew Johnson <itsandyjohn@gmail.com>
2022-10-27 12:23:07 -0600

    fixed path

--- components/account/orders/ItemDisplay.js
+++ components/account/orders/ItemDisplay.js
@@ -1,4 +1,4 @@
-import ProductImage from '../../atoms/ProductImage';
+import ProductImage from '../../products/ProductImage';
 import styled from 'styled-components';
 import formatMoney from '../../../lib/helpers/formatMoney';
 import { config } from '../../styles/GlobalStyles';
