0de82133300e3ae7cf00e0b8174b50663a8fcd48
Andrew Johnson <itsandyjohn@gmail.com>
2022-10-27 14:58:47 -0600

    test webhook

--- pages/api/webhooks.js
+++ pages/api/webhooks.js
@@ -1,11 +1,10 @@
 import Stripe from 'stripe';
 import { buffer } from 'micro';
-import createSubscription from '../../lib/webhook/createSubscription';
-import createOrder from '../../lib/webhook/createOrder';
 import deleteSubscription from '../../lib/webhook/deleteSubscription';
 import neboAxios from '../../config/axios';
 import { format } from 'date-fns';
 const orderid = require('order-id')('key');
+import axios from 'axios';
 
 export const config = {
   api: {
@@ -33,7 +32,7 @@ export default async function webhookHandler(req, res) {
 
       if (event.type === 'checkout.session.completed') {
         if (eventObject.subscription) {
-          const { amount_subtotal, created, customer, customer_details, metadata, subscription } = event.data.object;
+          const { amount_subtotal, created, customer, customer_details, metadata, subscription } = eventObject;
           const { current_period_end } = await stripe.subscriptions.retrieve(subscription);
 
           try {
@@ -66,12 +65,61 @@ export default async function webhookHandler(req, res) {
         }
 
         if (!eventObject.subscription) {
-          console.log('subscription created order');
           res.status(200).send();
-          await createOrder(eventObject, stripe);
-        }
-      }
+          const lineItems = await stripe.checkout.sessions.listLineItems(stripeObject.id);
 
+          const fetchProduct = async (name) => {
+            const { data } = await axios.get(
+              `${process.env.SANITY_URL}?query=*%5B_type%20%3D%3D%20%22product%22%20%26%26%20name%20%3D%3D%20'${name}'%5D%7B%0A%20%20%20%20_id%0A%20%20%0A%20%20%7D%20%20`
+            );
+
+            return data;
+          };
+
+          const { amount_subtotal, created, customer, payment_intent, customer_details } = eventObject;
+
+          async function getProductData() {
+            const data = await Promise.all(
+              lineItems.data.map(async (product) => {
+                const [name, texture] = product.description.split('-');
+                const data = await fetchProduct(name.replace(/\s/g, ''));
+
+                return { name: product.description, quantity: product.quantity, id: data.result[0]._id, texture };
+              })
+            );
+
+            await neboAxios.post('/', {
+              mutations: [
+                {
+                  create: {
+                    _type: 'orders',
+                    email: `${customer_details.email}`,
+                    name: `${customer_details.name}`,
+                    orderNumber: `${orderid.generate()}`,
+                    orderCreated: `${format(new Date(created * 1000), 'yyyy-MM-dd')}`,
+                    stripeDate: `${created}`,
+                    orderPrice: `${amount_subtotal}`,
+                    stripeUser: `${customer}`,
+                    stripeOrderID: `${payment_intent}`,
+                    shippingAddress: `${customer_details.address.line1} ${customer_details.address.city}, ${customer_details.address.state} ${customer_details.address.postal_code}`,
+                    orderItems: data.map((item) => {
+                      return {
+                        _key: item.id,
+                        quantity: item.quantity,
+                        product: { _ref: item.id },
+                        texture: item.texture,
+                      };
+                    }),
+                  },
+                },
+              ],
+            });
+          }
+        }
+
+        res.status(200).send();
+        await getProductData();
+      }
       if (event.type === 'customer.subscription.deleted') {
         res.status(200).send();
         await deleteSubscription(eventObject);
